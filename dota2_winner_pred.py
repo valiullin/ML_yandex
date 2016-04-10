d58eb2c6f5993693feadbc2fbe889dda9d7717ee
Adel Valiullin <adel.valiullin@start-i.ru>
2016-04-10 21:37:28 +0300

    init

---
+++ dota2_winner_pred.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+import pandas as pd
+
+__author__ = 'Adel'
+
+
+def boosting(dataframe):
+    pass
+
+if __name__ == '__main__':
+    data = pd.read_csv('data/features.csv', index_col='match_id')
+
+    boosting(data)
